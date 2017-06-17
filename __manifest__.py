29be974be00455ab5018169a9c675524d078fccf
Binh Dang <bdang2012@gmail.com>
2017-06-17 11:58:34 -0700

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    # Theme information
+    'name': "casting_theme",
+    'description': "Casting Theme",
+    'category': 'Theme',
+    'version': '1.0',
+    'depends': ['website'],
+
+    # templates
+    'data': [
+        'views/layout.xml',
+        'views/options.xml',
+        'views/snippets.xml',
+    ],
+
+
+    # Your information
+    'author': "Bovedalabs",
+    'website': "www.bovedalabs.com",
+}
